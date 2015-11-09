cd3cadc2df5788ef4c3d8fe0ed0074fc6c9e6d09
Brian Palmer <brianp@instructure.com>
2015-11-09 10:10:23 -0700

    only hardcode the brew path in CI builds
    necessary since homebrew isn't installed there.

--- cli/dinghy/constants.rb
+++ cli/dinghy/constants.rb
@@ -5,13 +5,17 @@ MEM_DEFAULT=2048
 CPU_DEFAULT=1
 DISK_DEFAULT=20_000
 
+if ENV['CI'] == 'true'
+  BREW = Pathname.new('/usr/local')
+else
+  BREW = Pathname.new(`brew --prefix`.strip)
+end
+
 # makes local dev easier
 if $0 =~ /bin\/_dinghy_command/ || $0 =~ /rspec/
-  BREW = Pathname.new('/usr/local')
   DINGHY = Pathname.new(File.expand_path("../..", File.dirname(__FILE__)))
   VAR = Pathname.new(File.expand_path("../../local/var", File.dirname(__FILE__)))
 else
-  BREW = Pathname.new(`brew --prefix`.strip)
   DINGHY = BREW+"opt/dinghy"
   VAR = BREW+"var/dinghy"
 end
