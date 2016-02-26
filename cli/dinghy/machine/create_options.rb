f4ab2ad4eb46f649e0ac10eabd2db74e59100f25
Ryan Schlesinger <ryan@outstand.com>
2016-02-26 05:51:11 -0800

    Add support for parallels and boot2docker-url

--- cli/dinghy/machine/create_options.rb
+++ cli/dinghy/machine/create_options.rb
@@ -5,6 +5,7 @@ module Machine::CreateOptions
       cpus: '--virtualbox-cpu-count',
       disk: '--virtualbox-disk-size',
       no_share: '--virtualbox-no-share',
+      boot2docker_url: '--virtualbox-boot2docker-url'
     }.freeze,
 
     'vmwarefusion' => {
@@ -12,6 +13,7 @@ module Machine::CreateOptions
       cpus: '--vmwarefusion-cpu-count',
       disk: '--vmwarefusion-disk-size',
       no_share: '--vmwarefusion-no-share',
+      boot2docker_url: '--vmwarefusion-boot2docker-url'
     }.freeze,
 
     'xhyve' => {
@@ -19,7 +21,16 @@ module Machine::CreateOptions
       cpus: '--xhyve-cpu-count',
       disk: '--xhyve-disk-size',
       # No shared folders are created on xhyve by default
+      boot2docker_url: '--xhyve-boot2docker-url'
     }.freeze,
+
+    'parallels' => {
+      memory: '--parallels-memory',
+      cpus: '--parallels-cpu-count',
+      disk: '--parallels-disk-size',
+      no_share: '--parallels-no-share',
+      boot2docker_url: '--parallels-boot2docker-url'
+    }.freeze
   }.freeze
 
   def self.generate(provider, options)
@@ -28,7 +39,12 @@ module Machine::CreateOptions
       flags[:memory], (options['memory'] || MEM_DEFAULT).to_s,
       flags[:cpus], (options['cpus'] || CPU_DEFAULT).to_s,
       flags[:disk], (options['disk'] || DISK_DEFAULT).to_s,
-      flags[:no_share],
-    ].compact
+      flags[:no_share]
+    ].compact.tap do |create_options|
+      unless options['boot2docker_url'].nil?
+        create_options << flags[:boot2docker_url]
+        create_options << options['boot2docker_url'].to_s
+      end
+    end
   end
 end
